6b5e95f4e13c8db41fee92426cc1fc89a719a402
Jesse <zhao.j.jesse@gmail.com>
2021-02-01 11:20:07 +1100

    updating cyclone

--- Bayesian MNNs/CyclonePrediction/util.py
+++ Bayesian MNNs/CyclonePrediction/util.py
@@ -3,6 +3,8 @@ import torch
 import torch.nn as nn
 import time
 import pyro
+from pyro.infer import SVI, Trace_ELBO, Predictive
+from pyro import poutine
 
 
 def load_data_as_tensor(data_folder="data/", use_gpu=False):
@@ -57,24 +59,61 @@ def gradient_descent(epochs, model, X_train, y_train, X_val, y_val, criterion=nn
     return model, train_losses, val_losses, early_stopping
 
 
+def variational_bayes(epochs, bayes_model, guide, X_train, y_train, X_val, y_val,
+                      patience=500, checkpoint_path='checkpoint.pt', num_particles=10):
+    adam = pyro.optim.Adam({'lr': 0.001})
+    svi = SVI(bayes_model, guide, adam, loss=Trace_ELBO(num_particles=num_particles))
+    early_stopping = EarlyStopping(patience=patience, path=checkpoint_path, bayes_model=True)
+    train_losses = []
+    val_losses = []
+
+    guide.requires_grad_(True)
+    start = time.time()
+    for i in range(epochs):
+        train_loss = svi.step(X_train, y_train) / len(y_train)
+        train_losses.append(train_loss)
+
+        val_loss = svi.evaluate_loss(X_val, y_val) / len(y_val)
+        val_losses.append(val_loss)
+
+        early_stopping(val_loss, None)
+        if early_stopping.early_stop:
+            print("Stopping early")
+            break
+
+        if (i + 1) % 250 == 0:
+            print(f"Done: {i + 1} / {epochs} epochs")
+            print(f"Train loss: {train_loss}")
+            print(f"Val loss: {val_loss}")
+            print(f"Time taken: {time.time() - start}s \n")
+
+    return train_losses, val_losses
+
+
+def get_posterior_samples(predictive, X, y):
+    with torch.no_grad():
+        posterior_sample = predictive(X, y)
+        posterior_losses = [nn.MSELoss()(y_pred, y).detach().item() for y_pred in posterior_sample['_RETURN']]
+    return posterior_sample, posterior_losses
+
+
 # https://github.com/Bjarten/early-stopping-pytorch/blob/master/pytorchtools.py
 class EarlyStopping:
     def __init__(self, patience=500, path='checkpoint.pt', bayes_model=False):
         self.patience = patience
         self.counter = 0
-        self.best_score = -np.Inf
+        self.best_val_loss = np.Inf
         self.early_stop = False
         self.path = path
         self.bayes_model = bayes_model
 
     def __call__(self, val_loss, model=None):
-        score = -val_loss
-        if score < self.best_score:
+        if val_loss > self.best_val_loss:
             self.counter += 1
             if self.counter >= self.patience:
                 self.early_stop = True
         else:
-            self.best_score = score
+            self.best_val_loss = val_loss
             self.save_checkpoint(model)
             self.counter = 0
 
@@ -83,13 +122,6 @@ class EarlyStopping:
             pyro.get_param_store().save(self.path)
         else:
             torch.save(model.state_dict(), self.path)
-
-
-
-def variational_bayes():
-    pass
-
-
 
 
 
